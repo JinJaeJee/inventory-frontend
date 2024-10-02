1bd2040da432cdbf31f069bf17e7a857304c38b3
JinJeaJee <jpluemphiromnat@gmail.com>
2024-10-02 12:00:39 +0700

    navbar and sidebar v1

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,7 +1,40 @@
+'use client'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector(state => state.global.isSidebarCollapsed)
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const SidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+  return (
+    <div className={SidebarClassNames}>
+      {/** Top Logo */}
+      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8 '>
+        <div>Logo</div>
+        <h1 className='font-extrabold text-2xl'>TokStock</h1>
+        <button className='md:hidden p-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
+          <Menu className='w-4 h-4' />
+        </button>
+      </div>
+
+      {/** LINKS */}
+      <div className='flex-grow mt-8'>{/** link here */}</div>
+
+      {/** Footer */}
+      <div>
+        <p className='text-center text-xs text-gray-500'>&copy; 2024 TokStock </p>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
