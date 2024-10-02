112b9d22aeaf32191ae3e6704506f475aeba499e
JinJeaJee <jpluemphiromnat@gmail.com>
2024-10-02 14:39:50 +0700

    nav v2

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useAppDispatch, useAppSelector } from '@/app/redux'
 import { setIsSidebarCollapsed } from '@/state'
-import { Menu } from 'lucide-react'
+import { Archive, CircleDollarSign, Clipboard, Layout, Menu, SlidersHorizontal, User } from 'lucide-react'
 import React from 'react'
+import SidebarLink from './SidebarLink'
 
 const Sidebar = () => {
   const dispatch = useAppDispatch()
@@ -18,19 +19,30 @@ const Sidebar = () => {
   return (
     <div className={SidebarClassNames}>
       {/** Top Logo */}
-      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8 '>
+      <div
+        className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? 'px-5' : 'px-8'
+        }`}
+      >
         <div>Logo</div>
-        <h1 className='font-extrabold text-2xl'>TokStock</h1>
+        <h1 className={`${isSidebarCollapsed ? 'hidden' : 'block'} font-extrabold text-2xl`}>TokStock</h1>
         <button className='md:hidden p-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
           <Menu className='w-4 h-4' />
         </button>
       </div>
 
       {/** LINKS */}
-      <div className='flex-grow mt-8'>{/** link here */}</div>
+      <div className='flex-grow mt-8'>
+        <SidebarLink href='/dashboard' icon={Layout} label='Dashboard' isCollapsed={isSidebarCollapsed} />
+        <SidebarLink href='/inventory' icon={Archive} label='Inventory' isCollapsed={isSidebarCollapsed} />
+        <SidebarLink href='/products' icon={Clipboard} label='Products' isCollapsed={isSidebarCollapsed} />
+        <SidebarLink href='/users' icon={User} label='Users' isCollapsed={isSidebarCollapsed} />
+        <SidebarLink href='/settings' icon={SlidersHorizontal} label='Settings' isCollapsed={isSidebarCollapsed} />
+        <SidebarLink href='/expenses' icon={CircleDollarSign} label='Expenses' isCollapsed={isSidebarCollapsed} />
+      </div>
 
       {/** Footer */}
-      <div>
+      <div className={`${isSidebarCollapsed ? 'hidden' : 'block'} mb-10`}>
         <p className='text-center text-xs text-gray-500'>&copy; 2024 TokStock </p>
       </div>
     </div>
